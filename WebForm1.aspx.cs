be179eff51523df4f4c3deda60c9e53684d25847
Đinh Nhật Nam <31850488+NhatNam-Kyoto@users.noreply.github.com>
2020-08-12 09:12:15 +0700

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication6
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connStr);
+            string sql = "SELECT * from Student";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            conn.Open(); //must be open
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            GridView1.DataSource = reader;
+            GridView1.DataBind();
+
+            conn.Close();
+        }
+    }
+}
---
+++ WebForm2.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication6
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string kytu = Request.QueryString["w"];
+            string sql = "SELECT * from Student where StudentId";
+            if (kytu != "") {
+                sql = "SELECT * from Student where StudentId = " + kytu;
+            }else
+            {
+                sql = "SELECT * from Student where StudentId = 1";
+            }
+            string connStr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connStr);
+            
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            GridView2.DataSource = reader;
+            GridView2.DataBind();
+
+            conn.Close();
+        }
+    }
+}
